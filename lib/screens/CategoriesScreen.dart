97e37cfdb1df8ae4442198218be736b82bbfb3ae
ayesh <ayeshlak1998@gmail.com>
2021-04-20 14:03:57 +0530

    Added alert dialog box in categories screen with category form

--- lib/screens/CategoriesScreen.dart
+++ lib/screens/CategoriesScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp_flutter/screens/homeScreen.dart';
 
 class Categories extends StatefulWidget {
   @override
@@ -6,11 +7,78 @@ class Categories extends StatefulWidget {
 }
 
 class _CategoriesState extends State<Categories> {
+  _showFormDialog(BuildContext context) {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: true,
+      builder: (param) {
+        return AlertDialog(
+          title: Text('Categories Form'),
+          content: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter Category',
+                    labelText: 'Category',
+                    // fillColor: Colors.white,
+                    filled: true,
+                    enabledBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Colors.white, width: 2.0)),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue, width: 2.0)),
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter Description',
+                    labelText: 'Description',
+                    // fillColor: Colors.white,
+                    filled: true,
+                    enabledBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Colors.white, width: 2.0)),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue, width: 2.0)),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('buttonText'),
+              onPressed: () {
+                Navigator.of(param).pop(); // Dismiss alert dialog
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (context) => HomeScreen()));
+          },
+          child: Icon(Icons.arrow_back),
+          style: ButtonStyle(elevation: MaterialStateProperty.all(0)),
+        ),
         title: Text('Categories'),
+      ),
+      body: Center(child: Text('Welcome to categories')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showFormDialog(context);
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
